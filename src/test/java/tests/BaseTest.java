be0270ded53fe141eeaeded4e4f92b0233e3cd57
Kate Yurm <yurmanovych@gmail.com>
2020-11-22 21:24:30 +0000

    added remote web driver

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,16 +1,24 @@
 package test.java.tests;
 
 
+import io.qameta.allure.Attachment;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import test.java.utils.PropertyLoader;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public abstract class BaseTest {
@@ -25,10 +33,19 @@ public abstract class BaseTest {
 //        chromeOptions.addArguments("--incognito");
 //        chromeOptions.addArguments("start-maximized");
 //        driver = new ChromeDriver(chromeOptions);
+//
+//        System.setProperty("webdriver.edge.driver", "src/main/resources/drivers/edge/v86/msedgedriver.exe");
+//        EdgeOptions edgeOptions = new EdgeOptions();
+//        driver=new EdgeDriver(edgeOptions);
 
-        System.setProperty("webdriver.edge.driver", "src/main/resources/drivers/edge/v86/msedgedriver.exe");
-        EdgeOptions edgeOptions = new EdgeOptions();
-        driver=new EdgeDriver(edgeOptions);
+        ChromeOptions chromeOptions = new ChromeOptions();
+        FirefoxOptions ffOptions = new FirefoxOptions();
+        try {
+            //driver = new RemoteWebDriver(new URL("http://ec2-3-250-219-25.eu-west-1.compute.amazonaws.com:4444/wd/hub"), ffOptions);
+            driver = new RemoteWebDriver(new URL("http://127.0.0.1:4444/wd/hub"), ffOptions);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
 
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.manage().window().maximize();
@@ -44,6 +61,19 @@ public abstract class BaseTest {
     @AfterMethod
     public void tearDown(ITestContext iTestContext) {
         //new test.java.utils.Screenshot(driver).takeScreenshot(testResult);
+        attachText();
+        attachScreen();
         driver.quit();
     }
+
+    @Attachment
+    private String attachText() {
+        return "Attached text";
+    }
+
+    @Attachment
+    private byte[] attachScreen() {
+        return ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
+    }
+
 }
